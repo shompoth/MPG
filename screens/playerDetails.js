e7ddbd33786591aad97bb4f7249eb63b11412171
Shompoth KHAN <dadoulan@gmail.com>
2021-07-13 10:46:43 +0200

    2nd axios get details players done

--- screens/playerDetails.js
+++ screens/playerDetails.js
@@ -1,7 +1,54 @@
-import React from "react";
+// Librairie
+import React, { useState, useEffect } from "react";
+import axios from "axios";
+import { View, Text, StyleSheet, ActivityIndicator } from "react-native";
 
-import { View, Text } from "react-native";
+function playerDetails(props) {
+    // State
+    const [detailPlayer, setDetailPlayer] = useState("");
 
-function playerDetails(props) {}
+    // Cycle de vie
+    useEffect(() => {
+        axios
+            .get(
+                `https://api.monpetitgazon.com/stats/player/${props.route.params.IDPlayer}?season=2018`,
+            )
+            .then(res => {
+                const player = res.data;
+                setDetailPlayer(player);
+            })
+            .catch(error => {
+                console.log(error);
+            });
+    }, []);
+
+    return (
+        <View style={styles.container}>
+            {detailPlayer ? (
+                <View style={styles.container}>
+                    <Text style={{ fontSize: 18 }}>{detailPlayer.firstname} </Text>
+                    <Text style={{ fontSize: 18 }}>{detailPlayer.lastname} </Text>
+                    <Text style={{ fontSize: 16, marginTop: 10 }}>
+                        {detailPlayer.club}{" "}
+                    </Text>
+                </View>
+            ) : (
+                <ActivityIndicator size="large" color="#000" />
+            )}
+        </View>
+    );
+}
+const styles = StyleSheet.create({
+    container: {
+        flex: 1,
+        alignItems: "center",
+        justifyContent: "center",
+        paddingTop: 10,
+        paddingBottom: 20,
+        backgroundColor: "#fff",
+        // paddingVertical: 50,
+        marginHorizontal: 15,
+    },
+});
 
 export default playerDetails;
